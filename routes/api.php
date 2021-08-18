20846a5755be7911c12f871f12f6b3ad7e574456
prapot <61309615+prapot@users.noreply.github.com>
2021-08-18 21:25:38 +0700

    add sleep

--- routes/api.php
+++ routes/api.php
@@ -48,7 +48,7 @@ Route::post('guide/soidow/{server?}', function ($server = null) {
             'status' => 200,
             'success' => 'true',
         ];
-        sleep(5);
+        sleep(25);
         Artisan::call("command:SendHuay");
         $message = [
             'status' => 200,
