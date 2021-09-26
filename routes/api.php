b005f0182470082332110c65b80d7501ccd88dd5
prapot <61309615+prapot@users.noreply.github.com>
2021-09-26 10:45:36 +0700

    down time

--- routes/api.php
+++ routes/api.php
@@ -51,7 +51,7 @@ Route::post('guide/soidow/{server?}', function ($server = null) {
             'status' => 200,
             'success' => 'true',
         ];
-        sleep(95);
+        sleep(70);
         Artisan::call("command:SendHuay");
         $message = [
             'status' => 200,
